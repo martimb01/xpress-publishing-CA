c312e45510a460b26bca12d8156bd658a1d4d265
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-01-10 20:40:43 +0000

    Initial DB schema created (migration.js)

---
+++ migration.js
@@ -0,0 +1,21 @@
+const sqlite3 = require('sqlite3');
+
+const db = new sqlite3.Database('./database.sqlite');
+
+db.run(`DROP TABLE IF EXISTS Artist`, (error)=> {
+    if (error) {
+        console.log('Something went wrong dropping the table')
+    } else {
+        console.log('Table has been dropped!')
+    }
+})
+
+db.run(`CREATE TABLE Artist (id INTEGER PRIMARY KEY NOT NULL,
+        name TEXT NOT NULL, date_of_birth TEXT NOT NULL,
+        biography TEXT NOT NULL, is_currently_employed INTEGER DEFAULT 1)`, (error) => {
+            if (error) {
+                console.log('Something went wrong creating the table')
+            } else {
+                console.log('Table has been created!')
+            }
+        })
--- server.js
+++ server.js
@@ -1,28 +1,28 @@
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
 const errorHandler = require('errorhandler');
-const morgan = require('morgan')
-const express = require('express');
+const apiRouter = require('./api/api');
 
-// Initialize the app
 const app = express();
 
 // Port the server will listen on
 const PORT = process.env.PORT || 4000;
 
-
 // Middleware for all routes
 app.use(bodyParser.json());
 app.use(cors());
-app.use(errorHandler());
 app.use(morgan('dev'));
+app.use('/api', apiRouter);
 
-//Start the server
-app.listen(PORT, ()=> {
-    console.log(`Server listening on port : ${PORT}`)
-})
+// Error handling middleware should be the last middleware
+app.use(errorHandler());
 
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
 
-//Export express app
+// Export express app
 module.exports = app;
-
